030ae95b26eac209ab58f388ca8fa374933ffb09
adsith <carlosalvarez1512@gmail.com>
2016-03-02 17:56:36 +0100

    Setting del Satge
    Se agregaron las paredes y el limit line.
    Se agregaron los dos players
    Se crearon los objetos de manejo del juego
    Se agregaron colores de referencia para los players

---
+++ Assets/Scripts/Enums/PlayerStates.cs
@@ -0,0 +1,8 @@
+﻿public enum PlayerStates
+{
+    idle, //Estado por default
+    moving, //Moviendose normalmente
+    usingChip, //En medio de la animacion de usar un chip
+    takingDamage, //En medio de la animacion de recibir daño
+    dead //En medio de la animacion de morir
+}
