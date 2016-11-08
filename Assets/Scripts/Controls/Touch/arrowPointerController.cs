d291e7b00902d8b148afb81346d1ae9d2c58063c
andreslaley <andreseloysv@gmail.com>
2016-11-08 21:11:31 +0100

    Touch
    Touch, nose porque la animacion no se activa

---
+++ Assets/Scripts/Controls/Touch/arrowPointerController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class arrowPointerController : MonoBehaviour {
+
+	// Use this for initialization
+    public GameObject Cone;
+    public GameObject Cylinder;
+    private Vector3 coneStartPosition;
+    private Vector3 cylinderStartPosition;
+    private bool bajando;
+    private bool termine;
+    private Vector3 coneTopPosition;
+    private Vector3 coneDownPosition;
+    private Vector3 cylinderTopPosition;
+    private Vector3 cylinderDownPosition;
+    float step;
+    float distancia;
+	void Start () {
+        
+        bajando = false;
+        distancia = 0.25f;
+        coneStartPosition = Cone.transform.position;
+        cylinderStartPosition = Cylinder.transform.position;
+
+        coneTopPosition = new Vector3(coneStartPosition.x,coneStartPosition.y+distancia,coneStartPosition.z);
+        coneDownPosition = new Vector3(coneStartPosition.x,coneStartPosition.y-distancia,coneStartPosition.z);
+        cylinderTopPosition = new Vector3(cylinderStartPosition.x,cylinderStartPosition.y+distancia,cylinderStartPosition.z);
+        cylinderDownPosition = new Vector3(cylinderStartPosition.x,cylinderStartPosition.y-distancia,cylinderStartPosition.z);
+	}
+	
+    void Update() 
+    {
+        step = Time.deltaTime/10;
+        if (bajando==true)
+        {
+            Cone.transform.position = Vector3.MoveTowards(Cone.transform.position, coneTopPosition, step);
+            Cylinder.transform.position = Vector3.MoveTowards(Cylinder.transform.position, cylinderTopPosition, step);
+        }
+        else
+        {
+            Cone.transform.position = Vector3.MoveTowards(Cone.transform.position, coneDownPosition, step);
+            Cylinder.transform.position = Vector3.MoveTowards(Cylinder.transform.position, cylinderDownPosition, step);
+        }
+
+        if (Cone.transform.position.y > 0.5)
+        {
+            bajando = false;
+        }else if(Cone.transform.position.y < 0.4f){
+            bajando = true;
+        }
+
+        transform.Rotate(Vector3.down * Time.deltaTime*100);
+
+
+    }
+    void OnCollisionEnter (Collision col)
+    {
+        Debug.Log(col.gameObject.name);
+        if(col.gameObject.name == "player1")
+        {
+            Renderer[] renderers = GetComponentsInChildren<Renderer>();
+            foreach (Renderer r in renderers)
+            {
+                r.enabled = false;
+            }
+
+            transform.position = new Vector3(0, 1, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Controls/Touch/playerController.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+    public float speed = 2f;
+    public float spacing = 0.1f;
+    public GameObject arrowPointer;
+    private Vector3 pos;
+    private Vector3 Liniepos;
+    private GameObject player1;
+    RaycastHit hit;
+
+	void Start () {
+        player1=GameObject.Find("Player1");
+        pos = transform.position;
+        //arrowPointer = Instantiate(Resources.Load("ArrowPointer/arrowpointer", typeof(GameObject))) as GameObject;
+
+	}
+	
+	void Update () {
+        //mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                try{
+                    pos=this.getScreenPosition(Input.mousePosition,transform.position.y);    
+                }catch(System.ArgumentException ex){}
+            }
+        }
+        else{
+            Vector3 heading = pos - transform.position;
+            //playerAnimator.Play();
+            if(heading.x>0){
+                player1.GetComponent<CharacterControl>().Move(1.0f, 0.0f);
+            }else if(heading.x<0){
+                player1.GetComponent<CharacterControl>().Move(-1.0f, 0.0f);
+            }else{
+                //player1.GetComponent<CharacterControl>().Move(0.0f, 0.0f);
+            }
+            if(heading.z>0){
+                player1.GetComponent<CharacterControl>().Move(0.0f, 1.0f);
+            }else if(heading.z<0){
+                player1.GetComponent<CharacterControl>().Move(0.0f, -1.0f);
+            }else{
+                //player1.GetComponent<CharacterControl>().Move(0.0f, 0.0f);
+            }
+
+
+            Vector3 targetDir = pos - transform.position;
+            float step = speed * Time.deltaTime;
+
+
+        }
+        //Touch
+        for (int i = 0; i < Input.touchCount; ++i) {
+            Touch touch = Input.GetTouch(i);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                try
+                {
+                    pos = this.getScreenPosition(touch.position,transform.position.y);
+                    arrowPointer.transform.position=new Vector3(pos.x,arrowPointer.transform.position.y,pos.z);
+                    Renderer[] renderers = arrowPointer.GetComponentsInChildren<Renderer>();
+                    foreach (Renderer r in renderers)
+                    {
+                        r.enabled = true;
+                    }
+
+                    Vector3 heading = pos - transform.position;
+                    //playerAnimator.Play();
+                    if(heading.x>0){
+                        player1.GetComponent<CharacterControl>().Move(1.0f, 0.0f);
+                    }else if(heading.x<0){
+                        player1.GetComponent<CharacterControl>().Move(-1.0f, 0.0f);
+                    }else{
+                        //player1.GetComponent<CharacterControl>().Move(0.0f, 0.0f);
+                    }
+                    if(heading.z>0){
+                        player1.GetComponent<CharacterControl>().Move(0.0f, 1.0f);
+                    }else if(heading.z<0){
+                        player1.GetComponent<CharacterControl>().Move(0.0f, -1.0f);
+                    }else{
+                        player1.GetComponent<CharacterControl>().Move(0.0f, 0.0f);
+                    }
+                }
+                catch (System.ArgumentException ex)
+                {
+                    //arrowPointer.GetComponent<Renderer>().enabled=false;
+                    Renderer[] renderers = arrowPointer.GetComponentsInChildren<Renderer>();
+                    foreach (Renderer r in renderers)
+                    {
+                        r.enabled = false;
+                    }
+                }
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                Liniepos=this.getScreenPosition(touch.position,transform.position.y);
+            }
+            else if(touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled){
+            }
+            else{
+            }
+        }
+
+	}
+
+    Vector3 getScreenPosition(Vector3 position,float currentY){
+        Ray ray = Camera.main.ScreenPointToRay(position);
+        if (Physics.Raycast(ray, out hit)&&(hit.transform.gameObject.tag.Equals("Player")||hit.transform.gameObject.tag.Equals("Stage")) )
+        {
+            return new Vector3(hit.point.x, currentY, hit.point.z);
+        }else{
+            throw new System.ArgumentException("No hit or no way", "original");
+             return new Vector3(0,0,0);
+        }
+    }
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Renderer[] renderers = arrowPointer.GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in renderers)
+        {
+            r.enabled = false;
+        }
+        // Code to execute after the delay
+    }
+}
